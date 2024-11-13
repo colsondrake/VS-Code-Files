c4d3dfcfc0283de73163064ad87545aa5a068030
Colson Drake <colsonjdrake@gmail.com>
2024-11-13 12:39:30 -0500

    Initial Sorting class commit

--- Sorting.java
+++ Sorting.java
@@ -1,3 +1,45 @@
 public class Sorting {
 
+    int[] array;
+
+    public Sorting() {
+        this.array = null;
+    } // Sorting
+
+    public Sorting(int[] inArr) {
+        this.array = inArr;
+    } // Sorting
+
+    public int[] getArray() {
+        return this.array;
+    } // getArray
+
+    public void setArray(int[] inArr) {
+        this.array = inArr;
+    } // setArray
+
+    public void print() {
+
+    } // print
+
+    public void selectionSort() {
+
+    } // selectionSort
+
+    public void mergeSort() {
+
+    } // mergeSort
+
+    public void heapSort() {
+
+    } // heapSort
+
+    public void quickSortLast() {
+
+    } // quickSortLast
+
+    public void quickSortRand() {
+
+    } // quickSortRand
+
 } // Sorting
